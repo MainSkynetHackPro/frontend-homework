0961cec51713d05d1e8b797b8fbf4a2b486deee3
Nikolay Yaminsky <yaminsky96@gmail.com>
2017-09-01 16:55:42 +0300

    fix use + removed var

--- source/plainify.js
+++ source/plainify.js
@@ -1,17 +1,17 @@
-'user strict';
+'use strict';
 
 
 function plainify(object) {
-    var data = {};
+    let data = {};
 
     function iterator(object, primary_index) {
-        var secondary_index = '';
+        let secondary_index = '';
 
         if (typeof primary_index !== 'undefined') {
             secondary_index = primary_index + '.';
         }
 
-        for (var item in object) {
+        for (let item in object) {
             if (typeof(object[item]) === 'object') {
                 iterator(object[item], secondary_index + item);
             } else {
